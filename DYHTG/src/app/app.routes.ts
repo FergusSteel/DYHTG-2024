9cbd927eec16601c0a19e635b0be08f01354b634
kylethmas <97603912+kylethmas@users.noreply.github.com>
2024-10-12 18:56:09 +0100

    Add Style, Harry Style

--- DYHTG/src/app/app.routes.ts
+++ DYHTG/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CallbackComponent } from './callback/callback.component';
+import { GraphComponent } from './graph/graph.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'callback', component: CallbackComponent},
+  { path: 'graph', component: GraphComponent},
 ];
--- DYHTG/src/app/graph/graph.component.ts
+++ DYHTG/src/app/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
 import { Node, Edge, ClusterNode } from '@swimlane/ngx-graph';
 
@@ -9,24 +9,23 @@ import { Node, Edge, ClusterNode } from '@swimlane/ngx-graph';
   templateUrl: './graph.component.html',
   styleUrl: './graph.component.css'
 })
-export class GraphComponent {
-	  public link_array: any;
-	  public node_array: any;
+export class GraphComponent implements OnInit {
+	public link_array: any;
+	public node_array: any;
 
-		constructor() {
-		  this.link_array = [{id: 'a', source: '1', target: '2'}, {id: 'b', source: '1', target: '3'}, {id: 'c', source: '3', target: '4'}, {id: 'd', source: '3', target: '5'}, {id: 'e', source: '4', target: '5'}, {id: 'f', source: '2', target: '6'}
-  ]
-	    this.node_array = [{id: '1', label: 'Node A'}, {id: '2', label: 'Node B'}, {id: '3', label: 'Node C'}, {id: '4', label: 'Node D'}, {id: '5', label: 'Node E'}, {id: '6', label: 'Node F'}];
-		}
+	constructor() {
+		this.link_array = [{id: 'a', source: '1', target: '2'}, {id: 'b', source: '1', target: '3'}, {id: 'c', source: '3', target: '4'}, {id: 'd', source: '3', target: '5'}, {id: 'e', source: '4', target: '5'}, {id: 'f', source: '2', target: '6'}]
+		this.node_array = [{id: '1', label: 'Node A'}, {id: '2', label: 'Node B'}, {id: '3', label: 'Node C'}, {id: '4', label: 'Node D'}, {id: '5', label: 'Node E'}, {id: '6', label: 'Node F'}];
+	}
 
-		getColour(n: any) : string{
-			return "#000FFF";
-		}
+	ngOnInit(): void {
+		// Logic to initialize and render the graph
+	}
+	getColour(n: any) : string{
+		return "#000FFF";
+	}
 
-		getImage(n: any) : string{
-			return "https://i.kym-cdn.com/photos/images/facebook/001/790/326/797.jpg";
-		}
-
-
-
+	getImage(n: any) : string{
+		return "https://i.kym-cdn.com/photos/images/facebook/001/790/326/797.jpg";
+	}
 }
