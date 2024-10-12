131af95584172e7bb68b12f8d5f7155394af3eef
FergusSteel <fergus.k.steel@gmail.com>
2024-10-12 10:41:23 +0100

    title change

--- DYHTG/src/app/app.component.spec.ts
+++ DYHTG/src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'DYHTG' title`, () => {
+  it(`should have the 'Crescendo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('DYHTG');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DYHTG');
+    expect(app.title).toEqual('Cresecendo');
   });
 });
--- DYHTG/src/app/app.component.ts
+++ DYHTG/src/app/app.component.ts
@@ -9,5 +9,11 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DYHTG';
+  title: string;
+
+
+  constructor() {
+    this.title = 'Crescendo';
+  }
+
 }
